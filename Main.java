6f2ba1f4251a6f03ec794c3e1887684f3760f483
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2023-05-26 11:07:31 -0300

    Refatoração no Main

--- Main.java
+++ Main.java
@@ -1,41 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Grafo g = new Grafo("G");
-        Grafo g2 = new Grafo("G2");
+        Boolean continuar = true;
+        int aux = 4;
+        Scanner sc = new Scanner(System.in);
+        Acoes acoes = new Acoes();
 
-        // Vértices de G
-        Vertice a = new Vertice("A");
-        Vertice b = new Vertice("B");
-        Vertice c = new Vertice("C");
-        Vertice d = new Vertice("D");
-        
-        g.adicionarVertice(a);
-        g.adicionarVertice(b);
-        g.adicionarVertice(c);
-        g.adicionarVertice(d);
+        while (continuar) {
+            acoes.mostrarMenu();
 
-        // Vértices de G2
-        Vertice e = new Vertice("E");
-        Vertice f = new Vertice("F");
-        Vertice gg = new Vertice("G");
-        Vertice h = new Vertice("H");
+            System.out.print("\nEscolha uma opção ");
+            aux = sc.nextInt();
 
-        
-        g2.adicionarVertice(e);
-        g2.adicionarVertice(f);
-        g2.adicionarVertice(gg);
-        g2.adicionarVertice(h);
+            switch (aux) {
+                case 1:
+                    acoes.adicionarVertice();
+                    continue;
 
-        // Arestas de G
-        g.adicionarAresta(a, b, "ab");
-        g.adicionarAresta(c, d, "cd");
+                case 2:
+                    acoes.adicionarAresta();
+                    continue;
 
-        // Arestas de G2
-        g2.adicionarAresta(e, f, "ef");
-        g2.adicionarAresta(gg, h, "gh");
+                case 3:
+                    acoes.mostrarGrafo();
+                    break;
 
-        g.retonarGrafo();
-        g2.retonarGrafo();
+                case 0:
+                    continuar = false;
+                    sc.close();
+                    break;
+
+                default:
+                    continue;
+            }
+        }
 
     }
+
 }
