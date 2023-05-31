08c7d8354c75ae0ac39ea7ebb357e409d8c19aa1
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2023-05-31 09:22:46 -0300

    Remoção do loop do Main

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,11 @@
 package src;
-import java.util.Scanner;
 // TODO: estudar manipulação de arquivo json com Java 
 
 public class Main {
     public static void main(String[] args) {
-        Boolean continuar = true;
-        int aux = 4;
-        Scanner sc = new Scanner(System.in);
-        Acoes acoes = new Acoes();
+       
 
-        while (continuar) {
-            acoes.mostrarMenu();
-
-            System.out.print("\nEscolha uma opção ");
-            aux = sc.nextInt();
-
-            switch (aux) {
-                case 1:
-                    acoes.adicionarVertice();
-                    continue;
-
-                case 2:
-                    acoes.adicionarAresta();
-                    continue;
-
-                case 3:
-                    acoes.mostrarGrafo();
-                    break;
-
-                case 0:
-                    continuar = false;
-                    sc.close();
-                    break;
-
-                default:
-                    continue;
-            }
-        }
+        
 
     }
 
