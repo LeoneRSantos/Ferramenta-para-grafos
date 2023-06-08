8094d9bc6d105e58f4a506794ef149e06977bc9b
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2023-06-07 23:30:32 -0300

    Mudança de Java para Python: classe de arestas

---
+++ Grafos/Aresta.py
@@ -0,0 +1,26 @@
+class Aresta:
+   def __init__(self, verticeUm, verticeDois, id):
+      self.setVerticeUm = verticeUm
+      self.setVerticeDois = verticeDois
+      self.setId = id
+
+   # Getters e setters
+   def setVerticeUm(self, verticeUm):
+      self.verticeUm = verticeUm
+
+   def getVerticeUm(self):
+      return self.verticeUm
+   
+   def setVerticeDois(self, verticeDois):
+      self.setVerticeDois = verticeDois
+
+   def getVerticeDois(self):
+      return self.VerticeDois
+
+   def setId(self, id):
+      self.id = id 
+   
+   def getId(self):
+      return self.id
+
+
