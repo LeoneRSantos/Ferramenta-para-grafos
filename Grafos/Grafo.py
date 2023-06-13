3b8cc65a86e1c1bc68893d588a701f32679efc25
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2023-06-13 13:29:34 -0300

    Modificações em Vértice e Grafo

--- Grafos/Grafo.py
+++ Grafos/Grafo.py
@@ -1,8 +1,12 @@
+from Aresta import Aresta
+from Vertice import Vertice
+
+
 class Grafo:
-    def __init__(self, nome, listaDeVertices, listaDeArestas):
-        self.setNome = nome
-        self.setVertices = listaDeVertices
-        self.setArestas = listaDeArestas
+    def __init__(self):
+        self.listaDeAdjacencia = []
+        self.listaDeVertices = []
+        self.listaDeArestas = []
 
     # Getter e setter
     def setNome(self, nome):
@@ -22,14 +26,39 @@ class Grafo:
 
     def getVertices(self):
         return self.vertices
+    
+    # Adicionar vértices e arestas
 
+    def adicionarVertices(self, vertices):
+        self.listaDeVertices = [Vertice(id) for id in vertices]
+        self.criarListaDeAdjacencia()
+
+    # Definir a lista de adjacência
+    def criarListaDeAdjacencia(self):
+        for i in self.listaDeVertices:
+            self.listaDeAdjacencia[i.nome] = []
+    
+    def adicionarArestas(self, arestas):
+        self.__verificarAdjacencia()
+        self.listaDeArestas = [
+            Aresta(aresta[0], aresta[1]) for aresta in enumerate(arestas)
+        ]
+
+    
 
     def __verificarAdjacencia(self):
         for aresta in self.__arestas:
-            self.__lista_de_Adjacentes[aresta.pontoA].append(
-                self.__select_vertice(aresta.pontoB)
+            self.listaDeAdjacencia[aresta.pontoA].append(
+                self.verificarVertice(aresta.pontoB)
             )
-            self.__lista_de_Adjacentes[aresta.pontoB].append(
-                self.__select_vertice(aresta.pontoA)
+            self.listaDeAdjacencia[aresta.pontoB].append(
+                self.verificarVertice(aresta.pontoA)
             )
+    
+    def verificarVertice(self, nome_vertice):
+        for vertice in self.__lista_de_Vertices:
+            if vertice.nome == nome_vertice:
+                return vertice
+        else:
+            return None
         
